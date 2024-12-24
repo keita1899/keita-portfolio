b3898c3f349b44d4c0a3ee63e884d211127e51a3
keita1899 <saboten18k@gmail.com>
2024-12-24 16:41:36 +0900

    MaterialUIを導入

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,13 +1,25 @@
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v14-pagesRouter'
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+export default function Document(props: DocumentHeadTagsProps) {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='ja'>
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   )
+}
+
+Document.getInitialProps = async (ctx: any) => {
+  const finalProps = await documentGetInitialProps(ctx)
+  return finalProps
 }
