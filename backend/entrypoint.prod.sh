a37ea82ffa7a10dfd1e0c5d1b5288d8249e6ab16
keita1899 <saboten18k@gmail.com>
2024-12-25 11:37:35 +0900

    seedも初回のデプロイ以外では実行しないようにする

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -8,12 +8,12 @@ rm -f /myapp/tmp/pids/server.pid
 
 if ! bundle exec rails db:version RAILS_ENV=production; then
   bundle exec rails db:create RAILS_ENV=production
+
+  echo "bundle exec rails db:seed RAILS_ENV=production"
+  bundle exec rails db:seed RAILS_ENV=production
 fi
 
 echo "bundle exec rails db:migrate RAILS_ENV=production"
 bundle exec rails db:migrate RAILS_ENV=production
-
-echo "bundle exec rails db:seed RAILS_ENV=production"
-bundle exec rails db:seed RAILS_ENV=production
 
 bundle exec rails s -b '0.0.0.0' -p 3000
