00256182d78c4959fb2b35509b27d82c84298bb6
keita1899 <saboten18k@gmail.com>
2024-12-27 02:25:55 +0900

    スキルのリストを表示するためのコンポーネント作成

---
+++ frontend/src/features/home/components/SkillItem.tsx
@@ -0,0 +1,27 @@
+import { Box, Grid, Typography } from '@mui/material'
+import Image from 'next/image'
+import { Skill } from '@/types/skill'
+
+type SkillItemProps = {
+  skill: Skill
+}
+
+export const SkillItem = ({ skill }: SkillItemProps) => {
+  return (
+    <Grid item xs={6} sm={3} md={2} key={skill.id}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Image src={skill.logo} alt="" width={64} height={64} />
+        <Typography variant="body2" sx={{ textAlign: 'center' }}>
+          {skill.name}
+        </Typography>
+      </Box>
+    </Grid>
+  )
+}
