66a94318af9a7243b03599fa55278031d3967788
keita1899 <saboten18k@gmail.com>
2024-12-27 02:24:59 +0900

    ポートフォリオ一覧を表示するためのカードとリストのコンポーネントを作成

---
+++ frontend/src/features/home/components/PortfolioCardList.tsx
@@ -0,0 +1,17 @@
+import { Grid } from '@mui/material'
+import { PortfolioCard } from './PortfolioCard'
+import { Portfolio } from '@/types/portfolio'
+
+type PortfolioCardListProps = {
+  portfolios: Portfolio[]
+}
+
+export const PortfolioCardList = ({ portfolios }: PortfolioCardListProps) => {
+  return (
+    <Grid container spacing={4}>
+      {portfolios.map((portfolio) => (
+        <PortfolioCard key={portfolio.id} portfolio={portfolio} />
+      ))}
+    </Grid>
+  )
+}
