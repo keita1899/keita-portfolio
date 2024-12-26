aae93652470c1ff0c6b229f7b07ccd15c89b7013
keita1899 <saboten18k@gmail.com>
2024-12-27 02:23:45 +0900

    ホームページで表示するセクションのコンポーネント作成

---
+++ frontend/src/features/home/components/Section.tsx
@@ -0,0 +1,39 @@
+import { Box } from '@mui/material'
+import { ReactNode } from 'react'
+import { SectionDescription } from './SectionDescription'
+import { SectionTitle } from './SectionTitle'
+import { TextAlignLayout } from '@/components/layouts/common/TextAlignContainer'
+
+type SectionProps = {
+  title: string
+  description?: string
+  backGroundColor?: string
+  children: ReactNode
+}
+
+export const Section = ({
+  title,
+  description,
+  backGroundColor,
+  children,
+}: SectionProps) => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: backGroundColor,
+        paddingY: 8,
+        minHeight: '500px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <TextAlignLayout align="center">
+        <SectionTitle title={title} />
+        {description && <SectionDescription description={description} />}
+      </TextAlignLayout>
+      {children}
+    </Box>
+  )
+}
