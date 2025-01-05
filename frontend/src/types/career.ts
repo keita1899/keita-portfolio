5a1c3d95eae74583ca53f51af07440f1429d0e6f
keita1899 <saboten18k@gmail.com>
2025-01-06 00:04:04 +0900

    feat: aboutページに必要な型を作成

---
+++ frontend/src/types/career.ts
@@ -0,0 +1,7 @@
+export type Career = {
+  id: number
+  organization: string
+  detail: string
+  startDate: string
+  endDate: string
+}
---
+++ frontend/src/types/profile.ts
@@ -0,0 +1,14 @@
+export type Profile = {
+  id: number
+  firstName: string
+  lastName: string
+  age: number
+  birthday: string
+  hometown: string
+  description: string
+  avatar: string
+  githubUrl: string
+  blogUrl: string
+  xUrl: string
+  qiitaUrl: string
+}
