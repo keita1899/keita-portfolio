5067b26067d7152b9a7bb50c70693b92af281767
keita1899 <saboten18k@gmail.com>
2025-01-07 21:14:08 +0900

    test: プロフィール詳細のリクエストスペック作成

---
+++ backend/spec/requests/profile_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Profiles API", type: :request do
+  describe "GET /show" do
+    it "プロフィールの情報を返す" do
+      user = User.create!(email: "test@example.com", password: "password")
+      Profile.create!(
+        user: user,
+        first_name: "慶太",
+        last_name: "濱崎",
+        age: 25,
+        birthday: "1999-02-15",
+        hometown: "兵庫県",
+        description: "Webエンジニアを目指して勉強中です。書籍やudemyで学習しています。主に Rails と React・TypeScript でポートフォリオを作成しています。",
+        avatar: "/images/avatar.png",
+        github_url: "https://github.com/",
+        blog_url: "https://keita1899.hatenablog.com/",
+        x_url: "https://x.com/k1e8i9t9a",
+        qiita_url: "https://qiita.com/keita1899",
+      )
+
+      get "/api/profile"
+
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)["first_name"]).to eq("慶太")
+    end
+  end
+end
