f47758146cee61810bcf019143f15e5ff87c3e27
keita1899 <saboten18k@gmail.com>
2025-01-13 21:48:52 +0900

    fix: データベースを一度リセットする

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -6,14 +6,17 @@ echo "Start entrypoint.prod.sh"
 echo "rm -f /myapp/tmp/pids/server.pid"
 rm -f /myapp/tmp/pids/server.pid
 
-if ! bundle exec rails db:version RAILS_ENV=production; then
-  bundle exec rails db:create RAILS_ENV=production
-fi
+# if ! bundle exec rails db:version RAILS_ENV=production; then
+#   bundle exec rails db:create RAILS_ENV=production
+# fi
 
-echo "bundle exec rails db:migrate RAILS_ENV=production"
-bundle exec rails db:migrate RAILS_ENV=production
+# echo "bundle exec rails db:migrate RAILS_ENV=production"
+# bundle exec rails db:migrate RAILS_ENV=production
 
-echo "bundle exec rails db:seed RAILS_ENV=production"
-bundle exec rails db:seed RAILS_ENV=production
+# echo "bundle exec rails db:seed RAILS_ENV=production"
+# bundle exec rails db:seed RAILS_ENV=production
+
+echo "bundle exec rails db:reset RAILS_ENV=production"
+bundle exec rails db:reset RAILS_ENV=production
 
 bundle exec rails s -b '0.0.0.0' -p 3000
