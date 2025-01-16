ea5b0e28568ca30f00217aa8043193135b33b9f3
keita1899 <saboten18k@gmail.com>
2025-01-16 20:28:27 +0900

    fix: 本番環境のデータベースをリセット

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -6,12 +6,14 @@ echo "Start entrypoint.prod.sh"
 echo "rm -f /myapp/tmp/pids/server.pid"
 rm -f /myapp/tmp/pids/server.pid
 
-if ! bundle exec rails db:version RAILS_ENV=production; then
-  bundle exec rails db:create RAILS_ENV=production
-fi
+# if ! bundle exec rails db:version RAILS_ENV=production; then
+#   bundle exec rails db:create RAILS_ENV=production
+# fi
 
-echo "bundle exec rails db:migrate RAILS_ENV=production"
-bundle exec rails db:migrate RAILS_ENV=production
+# echo "bundle exec rails db:migrate RAILS_ENV=production"
+# bundle exec rails db:migrate RAILS_ENV=production
+
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 
 echo "bundle exec rails db:seed RAILS_ENV=production"
 bundle exec rails db:seed RAILS_ENV=production
