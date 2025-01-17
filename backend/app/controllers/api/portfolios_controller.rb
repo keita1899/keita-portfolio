121ab1ead22db1a1f89725f1b0f1c5f8e510356b
keita1899 <saboten18k@gmail.com>
2025-01-17 20:37:44 +0900

    refactor: ユーザーの取得、ポートフォリオのincludesをリファクタリング

--- backend/app/controllers/api/portfolios_controller.rb
+++ backend/app/controllers/api/portfolios_controller.rb
@@ -1,33 +1,44 @@
 class Api::PortfoliosController < ApplicationController
   include UserFindable
 
-  def index
-    user = find_user
-    portfolios = user.portfolios.includes(:features, :pages, :images, :tech_stacks, :tags)
+  before_action :set_user
 
-    render json: portfolios, include: {
-      features: { only: [:id, :name] },
-      pages: { only: [:id, :name] },
-      images: { only: [:id, :url] },
-      tech_stacks: { only: [:id, :technology, :version] },
-      tags: { only: [:id, :name] },
-    }
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: "User not found" }, status: :not_found
+  def index
+    portfolios = @user.portfolios.includes(:features, :pages, :images, :tech_stacks, :tags)
+
+    render json: portfolios, include: portfolio_includes
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "User not found" }, status: :not_found
   end
 
   def show
-    user = find_user
-    portfolio = user.portfolios.includes(:features, :pages, :images, :tech_stacks, :tags).find(params[:id])
+    portfolio = @user.portfolios.includes(:features, :pages, :images, :tech_stacks, :tags).find_by(id: params[:id])
 
-    render json: portfolio, include: {
-      features: { only: [:id, :name] },
-      pages: { only: [:id, :name] },
-      images: { only: [:id, :url] },
-      tech_stacks: { only: [:id, :technology, :version] },
-      tags: { only: [:id, :name] },
-    }
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: "Portfolio not found" }, status: :not_found
+    if portfolio.nil?
+      render json: { error: "Portfolio not found" }, status: :not_found
+    else
+      render json: portfolio, include: portfolio_includes
+    end
+    
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "User not found" }, status: :not_found
   end
+
+  private
+
+    def set_user
+      @user = find_user
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "User not found" }, status: :not_found
+    end
+
+    def portfolio_includes
+      {
+        features: { only: [:id, :name] },
+        pages: { only: [:id, :name] },
+        images: { only: [:id, :url] },
+        tech_stacks: { only: [:id, :technology, :version] },
+        tags: { only: [:id, :name] }
+      }
+    end
 end
