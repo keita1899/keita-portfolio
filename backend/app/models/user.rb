4b213c3b449f493e77599860761c26a0c127e9ad
keita1899 <saboten18k@gmail.com>
2025-01-19 00:03:28 +0900

    feat: about関連のテーブルのリレーション作成

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :skills, dependent: :destroy
   has_many :portfolios, dependent: :destroy
+  has_many :carrers, dependent: :destroy
+  has_many :certifications, dependent: :destroy
+  has_many :hobbies, dependent: :destroy
 
   has_secure_password
 end
