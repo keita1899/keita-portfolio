c23ce0c7e1e7be16286dae02aae25df2bdd4715c
keita1899 <saboten18k@gmail.com>
2025-01-19 18:51:27 +0900

    fix: タイポ

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :skills, dependent: :destroy
   has_many :portfolios, dependent: :destroy
-  has_many :carrers, dependent: :destroy
+  has_many :careers, dependent: :destroy
   has_many :certifications, dependent: :destroy
   has_many :hobbies, dependent: :destroy
 
