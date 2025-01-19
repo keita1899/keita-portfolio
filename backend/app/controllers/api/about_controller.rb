ba6b4b64ecdd86a298f39193c3aa39251574f7a5
keita1899 <saboten18k@gmail.com>
2025-01-19 20:42:36 +0900

    feat: aboutのデータを返すAPIを作成

---
+++ backend/app/controllers/api/about_controller.rb
@@ -0,0 +1,21 @@
+class Api::AboutController < ApplicationController
+  include UserFindable
+
+  before_action :set_user
+
+  def index
+    profile = @user.profile
+    careers = @user.careers
+    certifications = @user.certifications
+    hobbies = @user.hobbies
+    render json: {
+      profile: profile.as_json(only: [:id, :last_name, :first_name, :age, :birthday, :hometown, :avatar, :description, :github_url, :blog_url, :x_url, :qiita_url]),
+      careers: careers.as_json(only: [:id, :organization, :detail, :start_date, :end_date]),
+      certifications: certifications.as_json(only: [:id, :name, :acquired_date]),
+      hobbies: hobbies.as_json(only: [:id, :name, :description])
+    }, status: :ok
+
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "User not found" }, status: :not_found
+  end
+end
