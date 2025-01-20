252ec5e14dbfe572a5179257f5863021d691c5fe
keita1899 <saboten18k@gmail.com>
2025-01-20 18:19:12 +0900

    feat: environmentsのseedを作成

---
+++ backend/db/seeds/environments.rb
@@ -0,0 +1,18 @@
+environments = [
+  {
+    label: 'OS',
+    name: 'MacOS',
+  },
+  {
+    label: 'エディタ',
+    name: 'VSCode、Cursor',
+  },
+  {
+    label: 'コマンドライン',
+    name: 'Warp',
+  },
+  {
+    label: 'ブラウザ',
+    name: 'Google Chrome、Arc',
+  },
+]
